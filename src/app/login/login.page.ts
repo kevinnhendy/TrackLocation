8bee9fb43b27e0b5a4e4308d95de0cb87a77a4e2
Kevin Hendy <kevin.hendy@student.umn.ac.id>
2020-12-15 22:42:15 +0700

    add login page scss

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -3,6 +3,7 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {Router} from '@angular/router';
 import {AuthService} from '../auth.service';
 import {AlertController, LoadingController} from '@ionic/angular';
+import {ConfirmPasswordValidator} from './confirm-password.validator';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +12,10 @@ import {AlertController, LoadingController} from '@ionic/angular';
 })
 export class LoginPage implements OnInit {
   form: FormGroup;
+  formSignUp: FormGroup;
   hide = true;
+  confirmHide = true;
+  passwordHide = true;
 
   constructor(
       private formBuilder: FormBuilder,
@@ -26,6 +30,28 @@ export class LoginPage implements OnInit {
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]]
     });
+
+    this.formSignUp = this.formBuilder.group({
+      firstname: ['', [Validators.required]],
+      lastname: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirm: ['', [Validators.required, Validators.minLength(6)]],
+    }, {
+      validator: ConfirmPasswordValidator('password', 'confirm'),
+    });
+
+    const container = document.querySelector('#container');
+    setTimeout(() => {
+      container.classList.add('sign-in')
+    }, 200);
+  }
+
+  toggle() {
+    const container = document.querySelector('#container');
+
+    container.classList.toggle('sign-in');
+    container.classList.toggle('sign-up');
   }
 
   async login() {
@@ -39,6 +65,25 @@ export class LoginPage implements OnInit {
       loading.dismiss();
       const alert = await this.alertController.create({
         header: 'Login akun gagal',
+        message: err.message,
+        buttons: ['OK'],
+      });
+
+      await alert.present();
+    });
+  }
+
+  async register() {
+    const loading = await this.loadingController.create();
+    await loading.present();
+
+    this.authService.register(this.formSignUp.value).then(user => {
+      loading.dismiss();
+      this.router.navigateByUrl('/menu/tabs/maps', { replaceUrl: true });
+    }, async err => {
+      loading.dismiss();
+      const alert = await this.alertController.create({
+        header: 'Registrasi akun gagal',
         message: err.message,
         buttons: ['OK'],
       });
